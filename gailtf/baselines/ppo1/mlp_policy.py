a7f93873c9669bfe92f09b370cefd231a892eef2
Xuan Tong <txmanialife@gmail.com>
2018-05-26 20:50:15 +1000

    update

--- gailtf/baselines/ppo1/mlp_policy.py
+++ gailtf/baselines/ppo1/mlp_policy.py
@@ -135,6 +135,42 @@ class MlpPolicy(object):
         self.ac = ac
         self._act = U.function([stochastic, ob], [ac, self.vpred])
 
+    # def act(self, stochastic, ob):
+    #     # print('~~~~',ob)
+
+    #     available_act_one_hot = ob[0][-524:]
+    #     # print(available_act_one_hot)
+    #     available_act = []
+    #     for i in range(0, len(available_act_one_hot)):
+    #         if available_act_one_hot[i] == 1.0:
+    #             available_act.append(i)
+
+    
+    #     # try to get valid action id,
+    #     loop_count = 0
+
+    #     ac1, vpred1 =  self._act(stochastic, ob)
+    #     actions_picked = [ac1[0]]
+    #     if available_act == []:
+    #         with open('act.log', 'a+') as f:
+    #             f.write('available_act is empty, return 0 as no_op')
+    #         return 0,vpred1[0] # no_op
+
+    #     while ac1[0] not in available_act:
+    #         # print('try to loop to get action in available_act: ', ac1[0])
+    #         ac1, vpred1 =  self._act(True, ob) # have to use stochastic
+    #         if loop_count > 50:
+    #             rdm_choice = random.choice(available_act)
+    #             with open('act.log', 'a+') as f:
+    #                 print("Cannot pick proper action, actions picked: {}, use {} keep on training, available_act are: {}".format(actions_picked, 
+    #                     rdm_choice, available_act), file = f)
+    #             return rdm_choice, vpred1[0]
+
+    #         loop_count += 1
+    #         actions_picked.append(ac1[0])
+
+    #     print('select action: ', ac1[0])
+    #     return ac1[0], vpred1[0]
     def act(self, stochastic, ob):
         # print('~~~~',ob)
 
@@ -144,32 +180,13 @@ class MlpPolicy(object):
         for i in range(0, len(available_act_one_hot)):
             if available_act_one_hot[i] == 1.0:
                 available_act.append(i)
-
-    
+        # print('available_act int mlp_policy.py act function: ', available_act)
         # try to get valid action id,
-        loop_count = 0
-
         ac1, vpred1 =  self._act(stochastic, ob)
-        actions_picked = [ac1[0]]
-        if available_act == []:
-            with open('act.log', 'a+') as f:
-                f.write('available_act is empty, return 0 as no_op')
-            return 0,vpred1[0] # no_op
-
         while ac1[0] not in available_act:
             # print('try to loop to get action in available_act: ', ac1[0])
             ac1, vpred1 =  self._act(True, ob) # have to use stochastic
-            if loop_count > 50:
-                rdm_choice = random.choice(available_act)
-                with open('act.log', 'a+') as f:
-                    print("Cannot pick proper action, actions picked: {}, use {} keep on training, available_act are: {}".format(actions_picked, 
-                        rdm_choice, available_act), file = f)
-                return rdm_choice, vpred1[0]
-
-            loop_count += 1
-            actions_picked.append(ac1[0])
-
-        print('select action: ', ac1[0])
+        print('~~~~~~ ac1[0]:', ac1[0])
         return ac1[0], vpred1[0]
     def get_variables(self):
         return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.scope)
