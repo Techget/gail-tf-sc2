a617d4a4c3996b71f8e6c695e24fb1bf50407aa2
Xuan Tong <txmanialife@gmail.com>
2018-05-26 10:29:12 +1000

    only choose from available action

--- gailtf/baselines/ppo1/mlp_policy.py
+++ gailtf/baselines/ppo1/mlp_policy.py
@@ -129,7 +129,7 @@ class MlpPolicy(object):
         # change for BC
         #stochastic = tf.placeholder(dtype=tf.bool, shape=())
         available_act = []
-        for i in range(0, len(available_action)):
+        for i in range(0, self.available_action_size):
             if available_action[i] == 1.0:
                 available_act.append(i)
 
