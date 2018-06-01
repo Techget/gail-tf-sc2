6490fc025030c782fee68eef5e0bf9fefe525c84
Xuan Tong <txmanialife@gmail.com>
2018-06-01 22:04:27 +1000

    remove space

--- gailtf/network/adversary.py
+++ gailtf/network/adversary.py
@@ -68,7 +68,7 @@ class TransitionClassifier(object):
     self.total_loss = generator_loss + expert_loss + entropy_loss
     # Build Reward for policy
     # make it larger, the network is large, it may vanish if reward is small
-    self.reward_op = 2 * (-tf.log(1-tf.nn.sigmoid(generator_logits)+1e-8)+(-tf.log(tf.nn.sigmoid(expert_logits)+1e-8))) 
+    self.reward_op = 2 * (-tf.log(1-tf.nn.sigmoid(generator_logits)+1e-8)+(-tf.log(tf.nn.sigmoid(expert_logits)+1e-8)))
     var_list = self.get_trainable_variables()
     self.lossandgrad = U.function([self.generator_obs_ph, self.generator_acs_ph, self.expert_obs_ph, self.expert_acs_ph], 
                          self.losses + [U.flatgrad(self.total_loss, var_list)])
