4db0176f7d36c79e2f104101950575f3e3bdfc25
Xuan Tong <txmanialife@gmail.com>
2018-06-11 11:28:42 +1000

    increase learning rate

--- gailtf/algo/trpo_mpi.py
+++ gailtf/algo/trpo_mpi.py
@@ -381,7 +381,7 @@ def learn(env, policy_func, discriminator, expert_dataset,
         gamma, lam, # advantage estimation
         entcoeff=0.001,
         cg_damping=1e-2,
-        vf_stepsize=3e-4, d_stepsize=3e-4,
+        vf_stepsize=3e-4, d_stepsize=5e-4,
         vf_iters =3,
         max_timesteps=0, max_episodes=0, max_iters=0, max_seconds=0,  # time constraint
         callback=None,
@@ -389,7 +389,7 @@ def learn(env, policy_func, discriminator, expert_dataset,
         load_model_path=None, task_name=None,
         timesteps_per_actorbatch=32,
         clip_param=0.15, adam_epsilon=3e-4,
-        optim_epochs=1, optim_stepsize=3e-4, optim_batchsize=32,schedule='linear'
+        optim_epochs=1, optim_stepsize=5e-4, optim_batchsize=32,schedule='linear'
         ):
     nworkers = MPI.COMM_WORLD.Get_size()
     print("##### nworkers: ",nworkers)
